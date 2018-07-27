9b6f6671ac569862351f7fa13f12ed6167f4c462
Keith Pearson <keith@qubitrot.org>
2018-07-26 22:43:02 -0700

    Add entry module

---
+++ src/Qubism.hs
@@ -0,0 +1,18 @@
+module Qubism 
+  ( module Qubism.QReg
+  , module Qubism.CReg
+  , module Qubism.QGate
+  , module GHC.TypeLits
+  , module Data.Singletons
+  , module Data.Singletons.TypeLits
+  , module Data.Finite
+  ) where
+
+import Qubism.QReg
+import Qubism.CReg
+import Qubism.QGate
+
+import GHC.TypeLits
+import Data.Singletons
+import Data.Singletons.TypeLits
+import Data.Finite
