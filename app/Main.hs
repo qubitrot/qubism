119a673d7aabc30e310801bd65c06fa1ddc2aa10
Keith Pearson <keith@qubitrot.org>
2018-07-29 16:40:42 -0700

    placeholder main

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import System.Environment
+import Text.Megaparsec
+
+import Qubism.QASM.Types
+import Qubism.QASM.Parser
+
 main :: IO ()
-main = undefined
+main = do
+  file   <- head <$> getArgs
+  source <- readFile file
+  case runParser mainprogram file source of
+    Left  err  -> putStr $ parseErrorPretty' source err
+    Right prog -> print prog
